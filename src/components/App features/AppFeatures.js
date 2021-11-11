f133ae628aa3460a403d3a3cff0c73041c003133
Albin <albinjiji4@gmail.com>
2021-11-11 08:30:59 +0530

    App section completed

--- src/components/App features/AppFeatures.js
+++ src/components/App features/AppFeatures.js
@@ -1,5 +1,48 @@
 import React from "react";
+import "./appfeatures.scss";
+import { AiFillApple } from "react-icons/ai";
+import img1 from "../Imges/rect.jpg";
 
 export default function AppFeatures() {
-  return <div>hello</div>;
+  return (
+    <div className="app">
+      <div className="left">
+        <h2 className="head">App Features</h2>
+        <p>Officia ex proident elit ex commodo aliqua nostrud aute ullamco.</p>
+        <p>
+          Ullamco id anim enim esse minim velit mollit enim culpa aute irure
+          magna sint dolore.
+        </p>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+        <div className="item">
+          <AiFillApple />
+          <h5>Coroporis Volupates sit</h5>
+        </div>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <img src={img1} />
+        </div>
+      </div>
+    </div>
+  );
 }
